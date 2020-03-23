f90592a49c8d18b113c5ae72401a4941bd1236ff
Germaine Pannell <me@germainep.com>
2020-03-22 21:02:53 -0400

    Login page working accepts login and forwards to the bubblepage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,29 @@
-import React { useState } from 'react'
+import React, { useState } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
-const Login = () => {
+const Login = ( props ) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  const [ credentials, setCredentails ] = useState({})
+  const [ credentials, setCredentials ] = useState({})
 
+  const Login = ( e ) => {
+    e.preventDefault()
+    axiosWithAuth().post('http://localhost:5000/api/login', credentials)
+                   .then(res => {
+                     console.log(credentials, res.data)
+                     localStorage.setItem('token', res.data.payload)
+                     props.history.push('/bubblepage')
+                   })
+  }
+  const handleChange = ( e ) => {
+    setCredentials({
+                     ...credentials,
+                     [e.target.name]: e.target.value
+                   })
+
+  }
   return (
-      <form onSubmit={ }>
+      <form onSubmit={ Login }>
         <h1>Welcome to the Bubble App!</h1>
         <label htmlFor="username">Username:
           <input name='username' id='username' value={ credentials.username }
