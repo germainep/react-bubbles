75f03b28acbb0fa96cd2a874ba71ccabcddb8021
Germaine Pannell <me@germainep.com>
2020-03-22 21:54:49 -0400

    implemented put call to api
    the api wasn't returning the updated data changed the server.js to send back the ColorList.js data

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -17,10 +17,14 @@ const ColorList = ({ colors, updateColors }) => {
   };
 
   const saveEdit = e => {
-    e.preventDefault();
+    e.preventDefault()
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${ colorToEdit.id }`, colorToEdit)
+                   .then(res => {
+                     updateColors([ ...res.data ])
+                   })
   };
 
   const deleteColor = color => {
